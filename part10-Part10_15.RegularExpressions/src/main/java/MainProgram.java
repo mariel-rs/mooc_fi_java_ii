ebab1416f86bf4421484500de443a550910c6cd2
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-11-16 23:02:54 -0600

    Progress part10 exercises

--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,13 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        String day = "mon";
+        String example = "aaaab";
+        String time = "04:59:31";
+
+        Checker checker = new Checker();
+        System.out.println(checker.isDayOfWeek(day));
+        System.out.println(checker.allVowels(example));
+        System.out.println(checker.timeOfDay(time));
     }
 }
